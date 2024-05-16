41935b482cbaeea81851eb2f674f63f9af003203
Paola_G <perfectblue@live.com>
2024-05-16 14:21:45 -0700

    added starter folders and installed dependencies

---
+++ src/app/(auth)/forgot-password/page.jsx
@@ -0,0 +1,3 @@
+export default function ForgotPasswordPage() {
+  return <div>forgot password page</div>;
+}
---
+++ src/app/blog/[slug]/page.jsx
@@ -0,0 +1,3 @@
+export default function SinglePostPage() {
+  return <div>Single post</div>;
+}
