bcb27b5ef10320ea6d510d5720b263df69bbf516
Paola_G <perfectblue@live.com>
2024-05-16 17:13:44 -0700

    added error page, loading page, error page that provides link to go back to Home page, Footer component, NavBar component with Links component, removed original styles and added a few new styles for body elements, added class container that wraps around NavBar, dynamic children, and Footer component in layout.js

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,12 @@
+import Links from "./links/Links";
+
+export default function NavBar() {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <Links></Links>
+      </div>
+    </div>
+  );
+}
